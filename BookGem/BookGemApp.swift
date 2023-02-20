3be81fe2b0303c98743ed1f464823f4ecd22fbcb
aselka27 <asjumalieva@gmail.com>
2023-02-20 09:44:18 +0600

    Initial Commit

---
+++ BookGem/BookGemApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookGemApp.swift
+//  BookGem
+//
+//  Created by саргашкаева on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookGemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookGem/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BookGem
+//
+//  Created by саргашкаева on 20.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
